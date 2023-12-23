bdf9d64715bf49f2fc04a965ffd06eca1d26b0ee
koratoo <ssyk2@naver.com>
2023-12-23 22:48:14 +0900

    섹션 1. 로그추적기
    VO, Service, Controller

---
+++ advanced/src/main/java/hello/advanced/aop/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package hello.advanced.aop.v0;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId){
+        if("ex".equals(itemId)){
+            throw new IllegalStateException();
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
