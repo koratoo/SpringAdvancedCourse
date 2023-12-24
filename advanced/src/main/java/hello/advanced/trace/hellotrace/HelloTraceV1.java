45ff35d77a494d67a23b782ff3811bc6227692e6
koratoo <ssyk2@naver.com>
2023-12-24 10:53:51 +0900

    로그추적기 V1 - 프로토타입 개발2

--- advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
+++ advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -9,23 +9,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloTraceV1 {
 
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<x-";
+
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", traceId.getId(), addSpace(), message);
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
-    private Object addSpace() {
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null){
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()),resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()),resultTimeMs);
+        }
     }
 
-    ;
-    public void end(TraceStatus status){
-
+    public void end(TraceStatus status) {
+        complete(status, null);
     };
 
-    public void exception(TraceStatus status, Exception e){
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    };
 
-    }
+    // level = 0
+    // level = 1 | -->
+    // level = 2 |    |-->
+    // level = 2 ex |    |<x-
+    // level = 1 ex |<x-
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level -1) ? "|" + prefix:"|   ");
+        }
+        return sb.toString();
+    };
+
 
 }
