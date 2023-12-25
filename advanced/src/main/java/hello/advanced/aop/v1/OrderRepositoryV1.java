c8033822e998bbd93357065cfadc312ee060909f
koratoo <ssyk2@naver.com>
2023-12-25 14:49:49 +0900

    로그추적기 V2 - 파라미터로 동기화 개발

--- advanced/src/main/java/hello/advanced/aop/v1/OrderRepositoryV1.java
+++ advanced/src/main/java/hello/advanced/aop/v1/OrderRepositoryV1.java
@@ -16,7 +16,7 @@ public class OrderRepositoryV1 {
         try {
             status = trace.begin("OrderRepositoryV1.save()");
             if(itemId.equals("ex")){
-                throw new IllegalStateException();
+                throw new IllegalStateException("예외 발생!!!");
             }
             sleep(1000);
             trace.end(status);
