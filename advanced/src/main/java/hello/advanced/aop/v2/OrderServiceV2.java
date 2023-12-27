45b52135b6fa9e08bb1131b3894ade00c0be17cc
koratoo <ssyk2@naver.com>
2023-12-27 19:17:51 +0900

    로그추적기 V2 - 적용

---
+++ advanced/src/main/java/hello/advanced/aop/v2/OrderServiceV2.java
@@ -0,0 +1,27 @@
+package hello.advanced.aop.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(String itemId){
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderServiceV1.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
